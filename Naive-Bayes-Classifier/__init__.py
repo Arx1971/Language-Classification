fdcd208c6c665abe564a60ffcab4958bd2452aa8
Arx1971 <adnan.rahin13@gmail.com>
2019-10-13 17:38:34 -0400

    Vocabulary Created For negative and Positive movie review

--- Naive-Bayes-Classifier/__init__.py
+++ Naive-Bayes-Classifier/__init__.py
@@ -1,4 +1,6 @@
 import os
+from string import punctuation
+import re
 
 
 def file_name_viewer(file_name):
@@ -6,6 +8,11 @@ def file_name_viewer(file_name):
         print(name)
 
     print(len(file_name))
+
+
+def dictionary_frequency_viewer(dictionary):
+    for values, key in dictionary.items():
+        print(values, key)
 
 
 def read_all_file_name(filepath):
@@ -16,10 +23,34 @@ def read_all_file_name(filepath):
     return files
 
 
+def set_vocabulary(review, filepath):
+    vocabulary = dict()
+    for review_name in review:
+        with open(filepath + review_name, "r") as reviews:
+            review = reviews.read()
+            words = re.split(r'[\s+`=~!@#$%^&*()_+\[\]{};\-\\:"|<,./<>?^]', review)
+            for word in words:
+                word = word.lower()
+                if len(word) is not 0:
+                    if word in vocabulary:
+                        vocabulary[word] += 1
+                    else:
+                        vocabulary[word] = 1
+    return vocabulary
+
+
+def training_vocabulary(neg_review, pos_review, filepath):
+    neg_vocabulary = set_vocabulary(neg_review, filepath + "/neg/")
+    pos_vocabulary = set_vocabulary(pos_review, filepath + "/pos/")
+
+    return {**neg_vocabulary, **pos_vocabulary}
+
+
 # Driver
 
 
 training_pos_file_name = read_all_file_name("../movie-review-HW2/aclImdb/train/pos")
 training_neg_file_name = read_all_file_name("../movie-review-HW2/aclImdb/train/neg")
 
-file_name_viewer(training_neg_file_name)
+dictionary = training_vocabulary(training_neg_file_name, training_pos_file_name, "../movie-review-HW2/aclImdb/train/")
+dictionary_frequency_viewer(dictionary)

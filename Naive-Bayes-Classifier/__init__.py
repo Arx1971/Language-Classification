c64e84754cf1ab7b8a3710858e745102cde80387
Arx1971 <adnan.rahin13@gmail.com>
2019-10-13 15:28:09 -0400

    File Parse is Done

--- Naive-Bayes-Classifier/__init__.py
+++ Naive-Bayes-Classifier/__init__.py
@@ -1,1 +1,25 @@
-print("TEST")
+import os
+
+
+def file_name_viewer(file_name):
+    for name in file_name:
+        print(name)
+
+    print(len(file_name))
+
+
+def read_all_file_name(filepath):
+    files = []
+    for i in os.listdir(filepath):
+        if i.endswith(".txt"):
+            files.append(i)
+    return files
+
+
+# Driver
+
+
+training_pos_file_name = read_all_file_name("../movie-review-HW2/aclImdb/train/pos")
+training_neg_file_name = read_all_file_name("../movie-review-HW2/aclImdb/train/neg")
+
+file_name_viewer(training_neg_file_name)
